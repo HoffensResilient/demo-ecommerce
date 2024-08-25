2dadb1bf467a4cc8f6ce1a7607e412f66d0d1f80
Rohit Raj Sarraf <horphanjones@gmail.com>
2024-08-25 14:36:23 +0545

    Initial commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function showleftsidebar() {
+    const leftsidebar = document.querySelector('.leftsidebar');
+    leftsidebar.style.display = 'flex';
+  }
+  
+  function showrightsidebar() {
+    const rightsidebar = document.querySelector('.rightsidebar');
+    rightsidebar.style.display = 'flex';
+  }
+  
+  function closebar() {
+    const leftsidebar = document.querySelector('.leftsidebar');
+    const rightsidebar = document.querySelector('.rightsidebar');
+    leftsidebar.style.display = 'none';
+    rightsidebar.style.display = 'none';
+  }
+  
